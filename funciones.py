4063eaedc074180936d0aad41f9818e424249410
Mate04 <matusmaldonado@hotmail.com>
2022-08-27 10:46:32 -0300

    1.1

--- funciones.py
+++ funciones.py
@@ -1,6 +1,6 @@
 from fractions import Fraction
-from colores import RED, WHITE, MAGENTA
-from colorama import Cursor, init, Fore
+from colores import RED, WHITE
+
 def printMatriz(matriz:list):
     nuevaMatriz = []
     for i in range(len(matriz)):
@@ -16,46 +16,28 @@ def Matriz(n:int,m:int):
         for j in range(m):
             vector[i].append(Fraction(input(f'Ingrese un numero para la fila {i+1} columna {j+1}: ')))
     return vector
-def verificarFila(matriz,fila):
-    if len(matriz) >= fila and fila > 0:
+def verificarFila(matriz:list,fila:int):
+    if len(matriz) > fila and fila >= 0:
         return True
     else:
         return False
 def interCambioDeFilas(matriz:list,filaA:int,filaB:int):
     if verificarFila(matriz,filaA) and verificarFila(matriz,filaB):
-        filaA -= 1
-        filaB -= 1
         matriz[filaA], matriz[filaB] = matriz[filaB],matriz[filaA]
-        print()
-        printMatriz(matriz)
-        print(Cursor.UP(1)+Cursor.FORWARD(20)+MAGENTA+f'E↓ {filaA+1} {filaB+1}'+WHITE)
-        print()
     else:
         print(RED+'No se puede, ya que no encontro una de las filas'+WHITE)
     return matriz
 def escalarXfila(matriz:list,fila:int,escalar:str):
     if verificarFila(matriz, fila):
-        fila -= 1
         for i in range(len(matriz[fila])):
             matriz[fila][i] *= escalar
-        print()
-        printMatriz(matriz)
-        print(Cursor.UP(1)+Cursor.FORWARD(20)+MAGENTA+f'E↓ {fila+1} ({escalar})'+WHITE)
-        print()
     else:
         print(RED+f'No se encontro la fila {fila}'+WHITE)
     return matriz
 def operacionEntreFila(matriz, filaB,filaA,escalar):
-    escalar = Fraction(escalar)
     if verificarFila(matriz, filaA) and verificarFila(matriz, filaB):
-        filaA -= 1
-        filaB -= 1
         for i in range(len(matriz[filaA])):
             matriz[filaB][i] += matriz[filaA][i] * escalar
-        print()
-        printMatriz(matriz)
-        print(Cursor.UP(1)+Cursor.FORWARD(20)+MAGENTA+f'E↓ {filaB+1} {filaA+1} ({escalar})'+WHITE)
-        print()
     else:
         print(RED+'No se puede, ya que no encontro una de las filas'+WHITE)
     return matriz
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+from colorama import Cursor, init, Fore
 from fractions import Fraction
 from funciones import printMatriz, Matriz, interCambioDeFilas, escalarXfila, operacionEntreFila
 from colores import YELLOW,CYAN, MAGENTA, BLUE, GREEN ,WHITE
@@ -6,25 +6,44 @@ from colores import YELLOW,CYAN, MAGENTA, BLUE, GREEN ,WHITE
 def main():
     print(GREEN+'='*20+'Cargar Matriz'+'='*20+WHITE);matriz = Matriz(int(input('ingrese la cantidad de filas: ')), int(input('ingrese la cantidad de columnas: ')))
     printMatriz(matriz)
-    print(CYAN+'='*60);opciones = int(input('Opcion 1: intercambio de fila\nOpcion 2: fila por un escalar\nOpcion 3: operacion elemental entre filas\nMostrar Matriz\nPresionar 0 para finalizar el programa\n opcion: '));print('='*60+WHITE)
+    print(CYAN+'='*60);opciones = int(input('Opcion 1: intercambio de fila\nOpcion 2: fila por un escalar\nOpcion 3: operacion elemental entre filas\n4- Mostrar Matriz\nPresionar 0 para finalizar el programa\n opcion: '));print('='*60+WHITE)
     while opciones != 0:
         printMatriz(matriz)
         if opciones == 1:
             print(YELLOW+'='*20+'Intercambio de Fila'+'='*20+WHITE)
-            matriz = interCambioDeFilas(matriz,int(input('Seleccionar una Fila: ')),int(input('seleccionar segunda Fila: ')))
+            fila1 = int(input('Seleccione una Fila para intercambiar: '))-1
+            fila2 = int(input('seleccionar segunda Fila para intercambiar: '))-1
+            matriz = interCambioDeFilas(matriz,fila1,fila2)
+            print()
+            printMatriz(matriz)
+            print(Cursor.UP(1)+Cursor.FORWARD(len(matriz[0])*10)+MAGENTA+f'E↓ {fila1+1} {fila2+1}'+WHITE)
+            print()
         if opciones == 2:
             print(MAGENTA+'='*20+'Escalar por fila'+'='*20+WHITE)
-            matriz = escalarXfila(matriz,int(input('que fila desea modificar: ')),Fraction(input('Escalar con el que desea operar: ')))
+            fila = int(input('que fila desea modificar: '))-1
+            escalar = Fraction(input('Escalar con el que desea operar: '))
+            matriz = escalarXfila(matriz,fila,escalar)
+            print()
+            printMatriz(matriz)
+            print(Cursor.UP(1)+Cursor.FORWARD(len(matriz[0])*10)+MAGENTA+f'E↓ {fila+1} ({escalar})'+WHITE)
+            print()
         if opciones == 3:
             print(BLUE+'='*20+'Operacion entre fila'+'='*20+WHITE)
-            fila2 = int(input('fila a modificar: '))
-            fila1 = int(input(f'La fila {fila2} se va sumar con la: '))
-            matriz = operacionEntreFila(matriz,fila2,fila1,input(f'escalar que va a multiplicar la fila {fila1}: '))
-        if opciones == 4:
+            fila2 = int(input('fila a modificar: '))-1
+            fila1 = int(input(f'La fila {fila2} se va sumar con la: '))-1
+            escalar = Fraction(input(f'escalar que va a multiplicar la fila {fila1+1}: '))
+            matriz = operacionEntreFila(matriz,fila2,fila1,escalar)
+            print()
             printMatriz(matriz)
+            print(Cursor.UP(1)+Cursor.FORWARD(len(matriz[0])*10)+MAGENTA+f'E↓ {fila2+1} {fila1+1} ({escalar})'+WHITE)
+            print()
+        if opciones == 4:
+            print(CYAN+'='*20+f'Matriz {len(matriz)}x{len(matriz[0])}'+'='*20+WHITE)
+            printMatriz(matriz)
+            input(f'\n{CYAN}+ presione enter para continuar')
         if opciones == 5:
             print(GREEN+'='*20+'Cargar Nueva Matriz'+'='*20+WHITE)
-            matriz = Matriz(int(input('ingrese el numero de fila de la nueva matriz')), int(input('ingrese el numero de columna de la nueva matriz: '))) 
+            matriz = Matriz(int(input('ingrese el numero de fila de la nueva matriz: ')), int(input('ingrese el numero de columna de la nueva matriz: '))) 
         #TODO: ciclo nuevo
         print(CYAN+'='*60);opciones = int(input('realizar otra operacion\nOpcion 1: intercambio de fila\nOpcion 2: fila por un escalar\nOpcion 3: Operacion elemental entre filas\nOpcion 4: Mostar Matriz\n5: Cargar nueva Matriz\nPresionar 0 para finalizar el programa\nOpcion: '));print('='*60+WHITE)
 main()
